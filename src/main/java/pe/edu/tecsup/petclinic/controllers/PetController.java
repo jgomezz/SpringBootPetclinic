ae52a57fb9ba717d5c28e50b89f44b7485793d0b
jgomezz <jgomezz@gmail.com>
2023-11-26 11:38:18 -0500

    Update

--- src/main/java/pe/edu/tecsup/petclinic/controllers/PetController.java
+++ src/main/java/pe/edu/tecsup/petclinic/controllers/PetController.java
@@ -1,9 +1,11 @@
 package pe.edu.tecsup.petclinic.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pet")
 public class PetController {
 
 	@GetMapping("/")
@@ -12,4 +14,25 @@ public class PetController {
 		return "Hola Mundo";
 	}
 	
+	
+	@GetMapping("/welcome") 
+	String welcome() {
+		return "Welcome to Tecsup";
+	}
+	
 }
+
+/*
+
+Crear la clase controlador VisitController
+y crear 2 metodo que respondan a la siguienets
+peticiones
+
+http://localhost:8080/visit   ---> Pagina principal de visitantes
+http://localhost:8080/visit/date   ---> Solicitud de cita
+
+*/
+
+
+
+
